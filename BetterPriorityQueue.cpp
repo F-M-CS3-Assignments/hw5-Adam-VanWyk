4f2c89dd050a20e5c088c3bafd82ffbdf85591e5
Adam-VanWyk <adam.c.vw@gmail.com>
2025-04-13 16:03:11 -0400

    starting BPQ

---
+++ BetterPriorityQueue.cpp
@@ -0,0 +1,22 @@
+
+
+
+#include "Graph.h"
+#include "BetterPriorityQueue.h"
+#include <vector>
+#include <queue>
+
+
+using namespace std;
+
+    // Returns true if BPQNode is in the queue
+bool contains(BPQNode);
+
+    // Returns true if any changes are actually made,
+    // tries to update the priority of a matching BPQNode in the queue
+bool update(BPQNode);
+
+    //  Returns a string of the whole queue as seen in pdf
+string ToString(vector<BPQNode>);
+
+static string BPQNodeToString(BPQNode);
