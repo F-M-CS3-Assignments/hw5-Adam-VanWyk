42a9635212f344776d308508aadd064c26846b79
Adam-VanWyk <adam.c.vw@gmail.com>
2025-04-14 16:34:13 -0400

    first try of update

--- BetterPriorityQueue.cpp
+++ BetterPriorityQueue.cpp
@@ -26,9 +26,25 @@ bool BetterPriorityQueue::Contains(BPQNode node){
     // Returns true if any changes are actually made,
     // tries to update the priority of a matching BPQNode in the queue
 bool BetterPriorityQueue::Update(BPQNode node){
+    if(!Contains(node)){
+        return false;
+    } 
+    bool found = false;
+    int index;
+    for (size_t i = 0; i < this->c.size(); i++){ //const auto& item : this->c
+        if (this->c.at(i).gnode == node.gnode && this->c.at(i).pri >= node.pri){
+            found = true;
+            index = i;
+            break;
+        }
+    }
+    if (!found){return found;} // if there is no matching again, double check
 
+    this->c.at(index).pri = node.pri;
 
-    return false;
+    make_heap(this->c.begin(), this->c.end(), this->comp); // from better priority queue
+
+    return true;
 }
 
     //  Returns a string of the whole queue as seen in pdf
