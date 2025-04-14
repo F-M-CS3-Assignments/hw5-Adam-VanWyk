2ac417171be460a061a251bfe28193509f097ab6
Adam-VanWyk <adam.c.vw@gmail.com>
2025-04-14 16:00:34 -0400

    .h file with correct implementation?

--- BetterPriorityQueue.h
+++ BetterPriorityQueue.h
@@ -2,23 +2,27 @@
 #include "Graph.h"
 #include <vector>
 #include <queue>
+#include <string>
 
 
 using namespace std;
 
-class BetterPriorityQueue: 
-    public priority_queue<BPQNode, vector<BPQNode>, greater<BPQNode>>::priority_queue {
+class BetterPriorityQueue {
+private: 
+    priority_queue<BPQNode, vector<BPQNode>, greater<BPQNode>> pq;
+
+public: 
 
     // Returns true if BPQNode is in the queue
-    bool contains(BPQNode);
+        bool contains(BPQNode);
 
     // Returns true if any changes are actually made,
     // tries to update the priority of a matching BPQNode in the queue
-    bool update(BPQNode);
+        bool update(BPQNode);
 
     //  Returns a string of the whole queue as seen in pdf
-    string ToString(vector<BPQNode>);
+        string ToString(vector<BPQNode>);
 
-    static string BPQNodeToString(BPQNode);
+        static string BPQNodeToString(BPQNode);
 
 };
