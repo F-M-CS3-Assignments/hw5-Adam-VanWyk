bc3edb3800db7eee521d5974781366ee3dc1838a
Adam-VanWyk <adam.c.vw@gmail.com>
2025-04-15 15:13:49 -0400

    corrected update re-insertion with push_heap, and did ToString()

--- BetterPriorityQueue.cpp
+++ BetterPriorityQueue.cpp
@@ -27,30 +27,38 @@ bool BetterPriorityQueue::Contains(BPQNode node){
     // tries to update the priority of a matching BPQNode in the queue
 bool BetterPriorityQueue::Update(BPQNode node){
     bool found = false;
-    int index;
+    //int index;
     for (size_t i = 0; i < this->c.size(); i++){
         if (this->c.at(i).gnode == node.gnode){
             if(node.pri < this->c.at(i).pri){
                 found = true;
-                index = i;
+                //index = i;
+                this->c.erase(this->c.begin() + i); // erase old node
+                this->c.push_back(node); // add new node
+                push_heap(this->c.begin(), this->c.end(), this->comp); // from better priority queue
             }
             break; // found node
         }
     }
     if (!found){return found;} // if there is no matching again, double check
 
-    this->c.at(index).pri = node.pri;
-
-    make_heap(this->c.begin(), this->c.end(), this->comp); // from better priority queue
-
-    return true;
+    return found;
 }
 
     //  Returns a string of the whole queue as seen in pdf
 string BetterPriorityQueue::ToString(){
+    string whole_bpq = "[";
 
-
-    return " ";
+    for (size_t i = 0; i < this->c.size(); i++)
+    {
+        if (i == this->c.size()-1){
+            whole_bpq = whole_bpq + "(" + to_string(this->c[i].gnode) + ", pri: " + to_string(this->c[i].pri) + ")";
+        } else{
+            whole_bpq = whole_bpq + "(" + to_string(this->c[i].gnode) + ", pri: " + to_string(this->c[i].pri) + "), ";
+        }
+    }
+    whole_bpq += "]";
+    return whole_bpq;
 }
 
 string BetterPriorityQueue::BPQNodeToString(BPQNode node){
